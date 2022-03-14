8c5822374a644071f93d18583867f0519368c989
becket <becket.bowes@gmail.com>
2022-03-14 12:42:15 -0400

    mvp on the backend

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,21 +1,33 @@
-const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const req = require('express/lib/request');
-const { Post, User, Vote } = require('../../models');
-const { route } = require('./user-routes');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote, Comment } = require('../../models');
 
+// get all users
 router.get('/', (req, res) => {
-  console.log('================');
+  console.log('======================');
   Post.findAll({
-    attributes: [
-    'id', 
-    'post_url', 
-    'title', 
-    'created_at',
-    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-  ],
     order: [['created_at', 'DESC']],
-    include: [{ model: User, attributes: ['username'] }]
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -26,19 +38,34 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   Post.findOne({
-    where: { id: req.params.id },
+    where: {
+      id: req.params.id
+    },
     attributes: [
-      'id', 
-      'post_url', 
-      'title', 
+      'id',
+      'post_url',
+      'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    include: [{ model: User, attributes: ['username'] }]
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
   })
     .then(dbPostData => {
       if (!dbPostData) {
-        res.status(404).json({ message: 'no post found with this id' });
+        res.status(404).json({ message: 'No post found with this id' });
         return;
       }
       res.json(dbPostData);
@@ -50,6 +77,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
@@ -74,12 +102,18 @@ router.put('/upvote', (req, res) => {
 
 router.put('/:id', (req, res) => {
   Post.update(
-    { title: req.body.title },
-    { where: { id: req.params.id } }
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
   )
     .then(dbPostData => {
       if (!dbPostData) {
-        res.status(404).json({ message: 'no post found with this id' });
+        res.status(404).json({ message: 'No post found with this id' });
         return;
       }
       res.json(dbPostData);
@@ -91,7 +125,11 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  Post.destroy({ where: { id: req.params.id } })
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
     .then(dbPostData => {
       if (!dbPostData) {
         res.status(404).json({ message: 'No post found with this id' });
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,13 +15,30 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-    include: [
-      { model: Post, attributes: ['id', 'title', 'post_url', 'created_at']},
-      { model: Post, attributes: ['title'], through: Vote, as: 'voted_posts' }
-  ],
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
+        model: Post,
+        attributes: ['title'],
+        through: Vote,
+        as: 'voted_posts'
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
