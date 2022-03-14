9cec2b2e3fcd2fb99eca0f0e34c70126b1a43459
becket <becket.bowes@gmail.com>
2022-03-13 22:07:35 -0400

    finished with vote model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,19 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 User.hasMany(Post, { foreignKey: 'user_id' });
 Post.belongsTo(User, { foreignKey: 'user_id' });
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
 
 module.exports = { User, Post };
